99b59af9e5be40fb8218d8a65ca9a1d7f2ebffe4
fabiowew <68655289+fabiowew@users.noreply.github.com>
2021-11-19 11:20:37 -0300

    ORM com JPA e Hibernate no JSF 2.2 e Persistência de dados - CRUD FINALIZADO

--- projetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
+++ projetojsf/src/main/java/br/com/cursojsf/PessoaBean.java
@@ -1,5 +1,9 @@
 package br.com.cursojsf;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
@@ -12,9 +16,15 @@ public class PessoaBean {
 
 	private Pessoa pessoa = new Pessoa();
 	private DaoGeneric<Pessoa> daoGeneric = new DaoGeneric<Pessoa>();
+	private List<Pessoa> pessoas = new ArrayList<Pessoa>();
+	
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
 	
 	public String salvar() {
 		pessoa = daoGeneric.merge(pessoa);
+		carregarPessoas();
 		return "";
 	}
 
@@ -42,6 +52,12 @@ public class PessoaBean {
 	public String remove() {
 		daoGeneric.deletePorId(pessoa);
 		pessoa = new Pessoa();
+		carregarPessoas();
 		return "";
+	}
+	
+	@PostConstruct
+	public void carregarPessoas() {
+		pessoas = daoGeneric.getListEntity(Pessoa.class);
 	}
 }
